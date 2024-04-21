904e2a7baa8f06aab6a42ef03d609a2f2c638509
GeantW0rld <106020834+GeantW0rld@users.noreply.github.com>
2024-04-21 12:58:12 +0200

    commit

---
+++ src/components/Test.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Test() {
+    return (
+        <div>
+            <h1>SkyBeWorld</h1>
+            <p>Hello World</p>
+        </div>
+    )
+}
